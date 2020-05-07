3d96aad03382837c7978538d6f651812015ecc7b
Hoang Sang <sagkg1998@gmail.com>
2020-05-07 18:49:42 +0700

    Login GUI

---
+++ app/src/main/java/com/example/ringvoip/Login/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.ringvoip.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ringvoip.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void btnLoginEvent(View view) {
+
+    }
+}
--- app/src/main/java/com/example/ringvoip/MainActivity.java
+++ app/src/main/java/com/example/ringvoip/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ringvoip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.ringvoip.Login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent_groupchat = new Intent(MainActivity.this, LoginActivity.class);
+//        intent_groupchat.putExtra("username", username);
+//        intent_groupchat.putExtra("sipuri", sipuri);
+        startActivity(intent_groupchat);
     }
 }
