ee5c281b43f8f1ab320e5cbcaf3bbf3653231559
Hoang Sang <sagkg1998@gmail.com>
2020-05-08 19:01:28 +0700

    Complete Service

--- app/src/main/java/com/example/ringvoip/Home/HomeActivity.java
+++ app/src/main/java/com/example/ringvoip/Home/HomeActivity.java
@@ -2,13 +2,16 @@ package com.example.ringvoip.Home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,9 +20,15 @@ import android.widget.TextView;
 
 import com.example.ringvoip.Chat.ChattingActivity;
 import com.example.ringvoip.Contact.ContactActivity;
+import com.example.ringvoip.LinphoneService;
+import com.example.ringvoip.Login.LoginActivity;
 import com.example.ringvoip.Profile.ProfileActivity;
 import com.example.ringvoip.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.linphone.core.CoreListenerStub;
+import org.linphone.core.ProxyConfig;
+import org.linphone.core.tools.Log;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -33,6 +42,8 @@ public class HomeActivity extends AppCompatActivity implements AdapterChatRoom.D
     ArrayList<ChatRoomClass> chatRoomList;
     String username, sipuri;
 
+    private CoreListenerStub mCoreListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +52,7 @@ public class HomeActivity extends AppCompatActivity implements AdapterChatRoom.D
         addEvents();
         addVariables();
         Intent intent = getIntent();
+
 //        username = intent.getStringExtra("username");
         username="sang";
         sipuri = intent.getStringExtra("sipuri");
@@ -55,6 +67,99 @@ public class HomeActivity extends AppCompatActivity implements AdapterChatRoom.D
         chatRoomList.add(chatRoomClass3);
         chatRoomList.add(chatRoomClass4);
         initView();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Ask runtime permissions, such as record audio and camera
+        // We don't need them here but once the user has granted them we won't have to ask again
+        checkAndRequestCallPermissions();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // The best way to use Core listeners in Activities is to add them in onResume
+        // and to remove them in onPause
+        LinphoneService.getCore().addListener(mCoreListener);
+
+        // Manually update the LED registration state, in case it has been registered before
+        // we add a chance to register the above listener
+        ProxyConfig proxyConfig = LinphoneService.getCore().getDefaultProxyConfig();
+        if (proxyConfig != null) {
+//            updateLed(proxyConfig.getState());
+        } else {
+//             No account configured, we display the configuration activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Like I said above, remove unused Core listeners in onPause
+        LinphoneService.getCore().removeListener(mCoreListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        // Callback for when permissions are asked to the user
+        for (int i = 0; i < permissions.length; i++) {
+            Log.i(
+                    "[Permission] "
+                            + permissions[i]
+                            + " is "
+                            + (grantResults[i] == PackageManager.PERMISSION_GRANTED
+                            ? "granted"
+                            : "denied"));
+        }
+    }
+
+    private void checkAndRequestCallPermissions() {
+        ArrayList<String> permissionsList = new ArrayList<>();
+
+        // Some required permissions needs to be validated manually by the user
+        // Here we ask for record audio and camera to be able to make video calls with sound
+        // Once granted we don't have to ask them again, but if denied we can
+        int recordAudio =
+                getPackageManager()
+                        .checkPermission(Manifest.permission.RECORD_AUDIO, getPackageName());
+        Log.i(
+                "[Permission] Record audio permission is "
+                        + (recordAudio == PackageManager.PERMISSION_GRANTED
+                        ? "granted"
+                        : "denied"));
+        int camera =
+                getPackageManager().checkPermission(Manifest.permission.CAMERA, getPackageName());
+        Log.i(
+                "[Permission] Camera permission is "
+                        + (camera == PackageManager.PERMISSION_GRANTED ? "granted" : "denied"));
+
+        if (recordAudio != PackageManager.PERMISSION_GRANTED) {
+            Log.i("[Permission] Asking for record audio");
+            permissionsList.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            Log.i("[Permission] Asking for camera");
+            permissionsList.add(Manifest.permission.CAMERA);
+        }
+
+        if (permissionsList.size() > 0) {
+            String[] permissions = new String[permissionsList.size()];
+            permissions = permissionsList.toArray(permissions);
+            ActivityCompat.requestPermissions(this, permissions, 0);
+        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener
